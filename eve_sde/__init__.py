c7f994b018852c7317a021e085c60f07b78f9fbb
Aaron Kable <aaronkable@gmail.com>
2026-01-05 20:19:17 +0800

    rename stuffs

--- eve_sde/__init__.py
+++ eve_sde/__init__.py
@@ -1,5 +1,5 @@
 """Django EvE SDE Models"""
 
 __version__ = "0.0.1a1"
-__title__ = "DjangoEveSDE"
+__title__ = "DjangoEvEOnlineSDE"
 __url__ = "https://github.com/Solar-Helix-Independent-Transport/django-eve-sde"
