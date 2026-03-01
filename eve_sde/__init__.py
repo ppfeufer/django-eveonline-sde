cecf5b5ba0fcfb26314235785b0f0ef434a1cc38
Aaron Kable <aaronkable@gmail.com>
2026-03-01 10:15:26 +0800

    beta 3

--- eve_sde/__init__.py
+++ eve_sde/__init__.py
@@ -1,5 +1,5 @@
 """Django EvE SDE Models"""
 
-__version__ = "0.0.1b2"
+__version__ = "0.0.1b3"
 __title__ = "DjangoEvEOnlineSDE"
 __url__ = "https://github.com/Solar-Helix-Independent-Transport/django-eveonline-sde"
