d2bc5b50d7e1200b3cf76e15b28c50ce88f28e48
T'rahk Rokym <trahk.rokym@gmail.com>
2026-03-04 22:05:47 +0100

    Move method down and mark protected

--- eve_sde/test_data.py
+++ eve_sde/test_data.py
@@ -11,18 +11,6 @@ from django.db.models import Model
 from eve_sde.models import JSONModel
 
 
-def return_all_parent_model_fields(model: type[Model]) -> list[type[Model]]:
-    """Returns all parent models of a given model recursively"""
-    parents = set()
-    for f in model._meta.fields:
-        if f.is_relation and f.related_model:
-            _f = getattr(model, f.name, None)
-            if _f:
-                parents.add(_f)
-                parents.update(return_all_parent_model_fields(_f))
-    return list(parents)
-
-
 @dataclass
 class ModelSpec:
     """Defines a model to be saved in a file for testing
@@ -53,7 +41,7 @@ def dump_model_data(specs: list[ModelSpec]) -> str:
             fields[f"{spec.field}__in"] = spec.ids
         qry = model.objects.filter(**fields)
         for mdl in qry:
-            parent_models += return_all_parent_model_fields(mdl)
+            parent_models += _return_all_parent_model_fields(mdl)
 
         parent_models.reverse()
         objects_to_dump.extend(parent_models)
@@ -75,6 +63,18 @@ def _get_model_class(spec: ModelSpec) -> type[JSONModel]:
         raise TypeError(f"Invalid model class {spec.model_name}")
 
     return model
+
+
+def _return_all_parent_model_fields(model: type[Model]) -> list[type[Model]]:
+    """Returns all parent models of a given model recursively"""
+    parents = set()
+    for f in model._meta.fields:
+        if f.is_relation and f.related_model:
+            _f = getattr(model, f.name, None)
+            if _f:
+                parents.add(_f)
+                parents.update(_return_all_parent_model_fields(_f))
+    return list(parents)
 
 
 def _remove_duplicates(objects_to_dump: list[type[Model]]):
